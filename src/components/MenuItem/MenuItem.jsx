49f49f9e206de0222d64cdc0945e999218b80ed4
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-03 20:20:57 +0600

    restructured folders, added image

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MenuItem.scss';
+
+
+const MenuItem = ({ title, imageUrl, size }) => (
+   <div className={`menu-item ${size}`} style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className="content">
+         <h1 className="title">{title}</h1>
+         <span className="subtitle">Shop Now</span>
+      </div>
+   </div>
+);
+
+export default MenuItem;
