c7e2abdf4c00265dd29488729fc3aa0f64794284
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-04 21:57:24 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 
@@ -8,6 +9,7 @@ import ShopPage from './pages/ShopPage/ShopPage';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
