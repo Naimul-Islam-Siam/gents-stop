960902bed1fe4e4081663f96e32f0c484418fdc6
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-05 02:57:18 +0600

    new route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import SignInPage from './pages/SignInPage/SignInPage';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInPage} />
       </Switch>
     </div>
   );
