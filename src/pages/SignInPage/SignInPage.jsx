b0ef29895d9ecde02c722d2d4498dcfca8825b4d
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-05 02:59:13 +0600

    Added signin form to signup page

---
+++ src/pages/SignInPage/SignInPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignInForm from '../../components/SignInForm/SignInForm';
+import './SignInPage.scss';
+
+const SignInPage = () => (
+   <div className="sign-in-and-sign-up">
+      <SignInForm />
+   </div>
+);
+
+export default SignInPage;
