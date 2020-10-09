3451f1197bb9238b6ddb1b03ec0c0a0ef5f8d47c
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-09 22:13:20 +0600

    add signup form and style

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignInForm from '../../components/SignInForm/SignInForm';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import './SignInPage.scss';
 
 const SignInPage = () => (
    <div className="sign-in-and-sign-up">
       <SignInForm />
+      <SignUpForm />
    </div>
 );
 
