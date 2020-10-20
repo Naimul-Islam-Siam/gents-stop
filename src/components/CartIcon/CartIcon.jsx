a4055b4493e2ca38e533ee958130e58fe4e77cbd
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-21 01:25:30 +0600

    show itemCount in cartIcon using selectCartItemsCount selector

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -3,12 +3,13 @@ import { ReactComponent as ShoppingIcon } from './../../assets/cart.svg';
 import './CartIcon.scss';
 import { connect } from 'react-redux';
 import { toggleCart } from './../../redux/cart/cartActions';
+import { selectCartItemsCount } from './../../redux/cart/cartSelectors';
 
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, itemCount }) => (
    <div className="cart-icon" onClick={toggleCart}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
    </div>
 );
 
@@ -16,4 +17,8 @@ const mapDispatchToProps = dispatch => ({
    toggleCart: () => dispatch(toggleCart())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+   itemCount: selectCartItemsCount(state) // allows CartIcon to re-render only when it's required (when state changes are related to itemCount)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
