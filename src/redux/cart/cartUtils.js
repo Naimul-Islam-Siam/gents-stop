fcc85c6ed452c6dd01a89a2a37c58517291ee828
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-21 19:48:58 +0600

    decrease item quantity from cart

--- src/redux/cart/cartUtils.js
+++ src/redux/cart/cartUtils.js
@@ -14,3 +14,19 @@ export const addItemToCart = (cartItems, itemToAdd) => {
    // if the item is not in cart yet, add it to cartItems and turn the quantity to 1
    return [...cartItems, { ...itemToAdd, quantity: 1 }];
 };
+
+
+export const decreaseItemFromCart = (cartItems, itemToRemove) => {
+   const existingCartItem = cartItems.find(cartItem => cartItem.id === itemToRemove.id);
+
+   // cause if the quantity is 1, the next decrease will remove the item from cart
+   if (existingCartItem.quantity === 1) {
+      return cartItems.filter(cartItem => cartItem.id !== itemToRemove.id);
+   }
+
+   return cartItems.map(cartItem =>
+      cartItem.id === itemToRemove.id ?
+         { ...cartItem, quantity: cartItem.quantity - 1 }
+         : cartItem
+   );
+};
