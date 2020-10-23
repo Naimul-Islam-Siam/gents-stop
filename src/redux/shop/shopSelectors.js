a100b1d5f2cc30a24cf991da1236c3c5d3925961
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-23 23:56:50 +0600

    collection page select from url

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -3,3 +3,17 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector([selectShop], shop => shop.collections);
+
+const COLLECTION_ID_MAP = {
+   shirts: 1,
+   sneakers: 2,
+   jackets: 3,
+   wathces: 4,
+   haircare: 5
+};
+
+export const selectCollectionFromUrl = collectionUrlParam => createSelector([selectCollections],
+   collections => collections.find(
+      collection =>
+         collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+   ));
