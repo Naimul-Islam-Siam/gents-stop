eebbdca57979c75ddfb3a1554062b919b6c21618
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-24 02:19:48 +0600

    change selectors to manage objects/hash instead of array

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -4,16 +4,11 @@ const selectShop = state => state.shop;
 
 export const selectCollections = createSelector([selectShop], shop => shop.collections);
 
-const COLLECTION_ID_MAP = {
-   shirts: 1,
-   sneakers: 2,
-   jackets: 3,
-   watches: 4,
-   haircare: 5
-};
+// convert object to iterable
+export const selectCollectionForPreview = createSelector([selectCollections],
+   collections => Object.keys(collections).map(key => collections[key])
+);
 
-export const selectCollectionFromUrl = collectionUrlParam => createSelector([selectCollections],
-   collections => collections.find(
-      collection =>
-         collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-   ));
+// route param
+export const selectCollectionFromUrl = collectionUrlParam =>
+   createSelector([selectCollections], collections => collections[collectionUrlParam]);
