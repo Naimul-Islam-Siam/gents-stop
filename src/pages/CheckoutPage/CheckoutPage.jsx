636f45b1062870da65adff7dd5fb9bcfad08c97e
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-10-26 01:05:17 +0600

    stripe checkout

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { selectCartItems } from './../../redux/cart/cartSelectors';
 import { selectCartTotalPrice } from './../../redux/cart/cartSelectors';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
+import StripeCheckoutButton from './../../components/StripeButton/StripeButton';
 
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -39,6 +40,14 @@ const CheckoutPage = ({ cartItems, total }) => (
       <div className="total">
          <span>Total: ${total}</span>
       </div>
+
+      <div className="test-warning">
+         *Please use the following test credit card for payments*
+         <br />
+         4242 4242 4242 4242 - Exp: 01/20 - CW: 123
+      </div>
+
+      <StripeCheckoutButton price={total} />
    </div>
 );
 
