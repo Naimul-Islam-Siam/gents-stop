de2a09fec79527adce3d5fc490cd50b64a28b01a
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-11-08 23:49:17 +0600

    root saga

---
+++ src/redux/rootSaga.js
@@ -0,0 +1,14 @@
+import { all, call } from 'redux-saga/effects';
+import { fetchCollectionsStart } from './shop/shopSagas';
+
+export default function* rootSaga() {
+   yield all([call(fetchCollectionsStart)]);
+
+   // we are using all instead of writing
+   // yield fetchCollectionsStart()
+   // yield fetchCollectionsStart()
+   // yield fetchCollectionsStart()
+   // because here 2nd one will depend on the first one to be resolved
+   // 3rd one will wait for the 2nd one to complete
+   // but we want them to initialize as soon as possible, that't why all is used
+};
