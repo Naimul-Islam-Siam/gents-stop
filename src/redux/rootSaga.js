9e377ec3e463b19ff7488a151146b1d95c24f30c
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-11-09 23:33:33 +0600

    add user sagas

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,8 +1,10 @@
 import { all, call } from 'redux-saga/effects';
 import { fetchCollectionsStart } from './shop/shopSagas';
+import { userSagas } from './user/userSagas';
+
 
 export default function* rootSaga() {
-   yield all([call(fetchCollectionsStart)]);
+   yield all([call(fetchCollectionsStart), call(userSagas)]);
 
    // we are using all instead of writing
    // yield fetchCollectionsStart()
