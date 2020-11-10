ff46752a474854d80fd1cbdf5da6892c82b65143
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2020-11-10 22:26:56 +0600

    shift email sign in to redux saga

--- src/redux/user/userSagas.js
+++ src/redux/user/userSagas.js
@@ -1,26 +1,53 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects';
 import { userActionTypes } from './userTypes';
-import { googleSigninSuccess, googleSigninFailure } from './userActions';
+import { signinSuccess, signinFailure } from './userActions';
 import { auth, googleProvider, createUserProfileDoc } from './../../firebase/firebase.utils';
+
+
+export function* getSnapshotFromAuth(userAuth) {
+   try {
+      const userRef = yield call(createUserProfileDoc, userAuth);
+
+      const userSnapshot = yield userRef.get();
+
+      yield put(signinSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+   } catch (error) {
+      yield put(signinFailure(error));
+   }
+};
+
+
 
 export function* signInWithGoogle() {
    try {
       const { user } = yield auth.signInWithPopup(googleProvider);
 
-      const userRef = yield call(createUserProfileDoc, user);
-
-      const userSnapshot = yield userRef.get();
-
-      yield put(googleSigninSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+      getSnapshotFromAuth(user);
    } catch (error) {
-      yield put(googleSigninFailure(error));
+      yield put(signinFailure(error));
    }
 };
+
+export function* signInWithEmail({ payload: { email, password } }) {
+   try {
+      const { user } = yield auth.signInWithEmailAndPassword(email, password);
+
+      getSnapshotFromAuth(user);
+   } catch (error) {
+      yield put(signinFailure(error));
+   }
+};
+
+
 
 export function* onGoogleSignInStart() {
    yield takeLatest(userActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 };
 
+export function* onEmailSignInStart() {
+   yield takeLatest(userActionTypes.GOOGLE_SIGN_IN_START, signInWithEmail);
+};
+
 export function* userSagas() {
-   yield all([call(onGoogleSignInStart)]);
+   yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
 };
